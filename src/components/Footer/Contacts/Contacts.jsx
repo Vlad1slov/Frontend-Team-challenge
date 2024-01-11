3eff5f544fda5cec5690a1a5eb06d28f9d0776c4
Denys07748 <denis07748@gmail.com>
2024-01-11 21:03:13 +0200

    adds Contacts html

---
+++ src/components/Footer/Contacts/Contacts.jsx
@@ -0,0 +1,18 @@
+export const Contacts = () => {
+  return (
+    <div>
+      <h3>Contacts</h3>
+      <ul>
+        <li>
+          <p>Holosiivsky ave, 89, Kyiv Ukraine</p>
+        </li>
+        <li>
+          <p>+38 (098) 128-49-45</p>
+        </li>
+        <li>
+          <p>homewell@gmail.com</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
