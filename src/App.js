5b9e32f40aa617c524952a5c0fa9b648e54db22e
Denys07748 <denis07748@gmail.com>
2024-01-11 21:01:18 +0200

    adds Footer html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
