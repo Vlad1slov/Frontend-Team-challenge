05d4f84ccc0a64de469c184f70cd6cc825487fa1
Denys07748 <denis07748@gmail.com>
2024-01-12 14:51:41 +0200

    adds Feedback-form html

--- src/components/Footer/Feedback/Feedback.jsx
+++ src/components/Footer/Feedback/Feedback.jsx
@@ -1,7 +1,39 @@
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import s from "./Feedback.module.css";
+
 export const Feedback = () => {
   return (
     <div>
       <h3>Get in touch</h3>
+      <Formik
+      // initialValues={initialValues}
+      // validationSchema={schema}
+      //   onSubmit={handleSubmit}
+      >
+        <Form className={s.form}>
+          <label htmlFor="name" className={s.label}>
+            Name
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" />
+          </label>
+          <label htmlFor="number" className={s.label}>
+            Phone number
+            <Field type="tel" name="number" />
+            <ErrorMessage name="number" component="div" />
+          </label>
+          <label htmlFor="email" className={s.label}>
+            Email
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component="div" />
+          </label>
+          <label htmlFor="message" className={s.label}>
+            Message
+            <Field type="text" name="message" />
+            <ErrorMessage name="message" component="div" />
+          </label>
+          <button type="submit">Send</button>
+        </Form>
+      </Formik>
     </div>
   );
 };
