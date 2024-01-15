d4db52513953aa5e8f140c385bf85540207caa6f
Vlad Chelombitko <vladmorilov24@gmail.com>
2024-01-15 07:20:25 -0600

    Adds Logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import "./App.css";
 import { Footer } from "./components/Footer/Footer";
-import MediaQuery from "react-responsive";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
-import MobileHeader from "./components/MobileHeader/MobileHeader";
 import AppRouter from "components/UI/AppRouter";
 
 function App() {
     return (
         <div className="App">
             <Router>
-                <MediaQuery minWidth={1280}>
-                    <Header />
-                </MediaQuery>
-                <MediaQuery maxWidth={1279}>
-                    <MobileHeader />
-                </MediaQuery>
+                <Header />
                 <Footer />
                 <AppRouter />
             </Router>
